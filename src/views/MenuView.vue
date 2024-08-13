9cbad3cfdc24df6ce25f85403ccd312b4db66729
Mickael <85728035+MickaelGP@users.noreply.github.com>
2024-08-13 12:09:42 +0200

    adding menu view

--- src/views/MenuView.vue
+++ src/views/MenuView.vue
@@ -1,5 +1,336 @@
+<script setup>
+import FooterComponent from '@/components/FooterComponent.vue';
+</script>
 <template>
     <main>
-        <h1>Menu</h1>
+        <section id="cocktails">
+            <div class="container">
+                <div class="text-center py-5">
+                    <span class="sub-title">Menu</span>
+                    <h3>Cocktails</h3>
+                </div>
+                <div class="row mx-0 py-5">
+                    <div class="col-lg-6 mt-3">
+                        <div class="cocktail">
+                            <div class="d-flex justify-content-between">
+                                <div class="cocktail-title">
+                                    <h5>Classic Mojito</h5>
+                                </div>
+                                <div class="cocktail-line"> </div>
+                                <div class="cocktail-price">
+                                    <span class="mx-2">14$</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <span class="cocktail-description">Rhum, Citron Vert, Menthe Fraîche, Sucre roux, Eau gazeuse</span>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 mt-3">
+                        <div class="cocktail">
+                            <div class="d-flex justify-content-between">
+                                <div class="cocktail-title">
+                                    <h5>Mojito Exotique</h5>
+                                </div>
+                                <div class="cocktail-line"> </div>
+                                <div class="cocktail-price">
+                                    <span class="mx-2">14$</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <span class="cocktail-description">Rhum, Purée de Passion, Mangue, Ananas, Citron Vert, Menthe Fraîche, Sucre roux, Eau gazeuse</span>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 mt-3">
+                        <div class="cocktail">
+                            <div class="d-flex justify-content-between">
+                                <div class="cocktail-title">
+                                    <h5>Apérol Spritz</h5>
+                                </div>
+                                <div class="cocktail-line"> </div>
+                                <div class="cocktail-price">
+                                    <span class="mx-2">14$</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <span class="cocktail-description">Apérol, Prosecco, Eau gazeuse</span>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 mt-3">
+                        <div class="cocktail">
+                            <div class="d-flex justify-content-between">
+                                <div class="cocktail-title">
+                                    <h5>Galet’s Spritz</h5>
+                                </div>
+                                <div class="cocktail-line"> </div>
+                                <div class="cocktail-price">
+                                    <span class="mx-2">14$</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <span class="cocktail-description">Apérol, Jus d’Orange Pressée, Prosecco, Sirop de Sucre</span>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 mt-3">
+                        <div class="cocktail">
+                            <div class="d-flex justify-content-between">
+                                <div class="cocktail-title">
+                                    <h5>Sex on the Beach</h5>
+                                </div>
+                                <div class="cocktail-line"> </div>
+                                <div class="cocktail-price">
+                                    <span class="mx-2">14$</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <span class="cocktail-description">Vodka, Crème de Pêche, Jus d’Orange, Jus de Cranberries</span>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 mt-3">
+                        <div class="cocktail">
+                            <div class="d-flex justify-content-between">
+                                <div class="cocktail-title">
+                                    <h5>Caïpirinha</h5>
+                                </div>
+                                <div class="cocktail-line"> </div>
+                                <div class="cocktail-price">
+                                    <span class="mx-2">14$</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <span class="cocktail-description">Cachaça, Citron Vert, Cassonade</span>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 mt-3">
+                        <div class="cocktail">
+                            <div class="d-flex justify-content-between">
+                                <div class="cocktail-title">
+                                    <h5>Moscow Mule</h5>
+                                </div>
+                                <div class="cocktail-line"> </div>
+                                <div class="cocktail-price">
+                                    <span class="mx-2">16$</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <span class="cocktail-description">Vodka, Ginger Beer, Jus de Citron Vert</span>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 mt-3">
+                        <div class="cocktail">
+                            <div class="d-flex justify-content-between">
+                                <div class="cocktail-title">
+                                    <h5>Mary Pickford</h5>
+                                </div>
+                                <div class="cocktail-line"> </div>
+                                <div class="cocktail-price">
+                                    <span class="mx-2">16$</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <span class="cocktail-description">Rhum cubain, liqueur marasquin, ananas, citron vert, grenadine</span>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 mt-3">
+                        <div class="cocktail">
+                            <div class="d-flex justify-content-between">
+                                <div class="cocktail-title">
+                                    <h5>Cosmopolitan</h5>
+                                </div>
+                                <div class="cocktail-line"> </div>
+                                <div class="cocktail-price">
+                                    <span class="mx-2">16$</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <span class="cocktail-description">Rye Whisky, vermouth rouge, Angostura Bitters</span>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 mt-3">
+                        <div class="cocktail">
+                            <div class="d-flex justify-content-between">
+                                <div class="cocktail-title">
+                                    <h5>Margarita</h5>
+                                </div>
+                                <div class="cocktail-line"> </div>
+                                <div class="cocktail-price">
+                                    <span class="mx-2">14$</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <span class="cocktail-description">Apérol, Prosecco, Eau gazeuse</span>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 mt-3">
+                        <div class="cocktail">
+                            <div class="d-flex justify-content-between">
+                                <div class="cocktail-title">
+                                    <h5>Apérol Spritz</h5>
+                                </div>
+                                <div class="cocktail-line"> </div>
+                                <div class="cocktail-price">
+                                    <span class="mx-2">16$</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <span class="cocktail-description">Vodka, Cointreau, cranberries, citron vert</span>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 mt-3">
+                        <div class="cocktail">
+                            <div class="d-flex justify-content-between">
+                                <div class="cocktail-title">
+                                    <h5>Royal</h5>
+                                </div>
+                                <div class="cocktail-line"> </div>
+                                <div class="cocktail-price">
+                                    <span class="mx-2">20$</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <span class="cocktail-description">Kirsch, sirop de framboise, zeste d’orange, Champagne Blanc de Blanc, paillettes d’or</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="image-cocktail"></section>
+
+        <section id="whisky" class="bg-black">
+            <div class="container">
+                <div class="text-center py-5">
+                    <span class="sub-title">Menu</span>
+                    <h3>Whisky</h3>
+                </div>
+                <div class="row mx-0 py-5">
+                    <div class="col-lg-6 mt-3">
+                        <div class="whisky">
+                            <div class="d-flex justify-content-between">
+                                <div class="whisky-title">
+                                    <h5>Macallan</h5>
+                                </div>
+                                <div class="whisky-line"> </div>
+                                <div class="whisky-price">
+                                    <span class="mx-2">14$</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <span class="whisky-description">12 ans d'age triple cask matured  6cl </span>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 mt-3">
+                        <div class="whisky">
+                            <div class="d-flex justify-content-between">
+                                <div class="whisky-title">
+                                    <h5>Macallan</h5>
+                                </div>
+                                <div class="whisky-line"> </div>
+                                <div class="whisky-price">
+                                    <span class="mx-2">16$</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <span class="whisky-description">12 ans d'age sherry oak cask  6cl </span>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 mt-3">
+                        <div class="whisky">
+                            <div class="d-flex justify-content-between">
+                                <div class="whisky-title">
+                                    <h5>Rozelieures</h5>
+                                </div>
+                                <div class="whisky-line"> </div>
+                                <div class="whisky-price">
+                                    <span class="mx-2">12$</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <span class="whisky-description">Single malt origine collection 6cl </span>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 mt-3">
+                        <div class="whisky">
+                            <div class="d-flex justify-content-between">
+                                <div class="whisky-title">
+                                    <h5>Oban</h5>
+                                </div>
+                                <div class="whisky-line"> </div>
+                                <div class="whisky-price">
+                                    <span class="mx-2">14$</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <span class="whisky-description">14 ans d'age tourbé  6cl </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="image-whisky"></section>
     </main>
+    <FooterComponent />
 </template>
+<style scoped>
+footer {
+    /* position: absolute; */
+    bottom: 0;
+}
+
+.sub-title {
+    color: var(--primary-color);
+}
+
+#cocktails {
+    background-color: var(--secondary-color);
+    color: white;
+}
+.cocktail-title h5, .whisky-title h5{
+    padding-right: 0.5rem;
+}
+.cocktail-line, .whisky-line {
+    height: 1px;
+    width: 100%;
+    border-bottom: 1px solid var(--border-color);
+    flex:1;
+    position: relative;
+    bottom: -15px;
+}
+.cocktail-description, .whisky-description{
+    font-size: 0.8rem;
+    color: var(--primary-color);
+}
+.image-cocktail{
+    width: 100%;
+    padding: 15rem;
+    background-image: url('../assets/img/cocktail.jpg');
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+}
+#whisky{
+    color: white;
+}
+.image-whisky{
+    width: 100%;
+    padding: 15rem;
+    background-image: url('../assets/img/whisky.jpg');
+    background-position: center;
+    background-size: cover;
+}
+</style>
